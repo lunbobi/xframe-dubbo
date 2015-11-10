0ecda95e91febb4d9bb22ee5ee53b2d7393f838a
deane163 <deane163@126.com>
2015-11-10 08:54:08 +0800

    dubbo 基础架构

---
+++ src/main/java/com/xinyi/xframe/xframe_dubbo/Application.java
@@ -0,0 +1,17 @@
+/**
+*
+*/
+package com.xinyi.xframe.xframe_dubbo;
+
+/**
+ * 功能说明：作为Dubbo服务的基础类，详细请参考readme文档
+ * 
+ * Application.java
+ * 
+ * Original Author: liangliang.jia,2015年7月20日下午3:11:24
+ * 
+ * Copyright (C)1997-2015 深圳信义科技 All rights reserved.
+ */
+public interface Application {
+
+}
